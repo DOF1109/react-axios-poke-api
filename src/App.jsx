9ba6aa40ab09c7de06cc5bb2f0451cf3ddeec6a1
DOF1109 <friasdaniel.1109@gmail.com>
2024-03-12 11:19:58 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Pokemon from './components/Pokemon';
+
+function App() {
+  return (
+    <>
+      <h1>APP</h1>
+      <Pokemon />
+    </>
+  );
+}
+
+export default App;
